4c34ca11ddcb484027f27d04f4b8b95f76c51e9f
arcasoftstudio <arcasoftstudio@gmail.com>
2025-04-24 16:10:48 +0300

    Create unzip.py

---
+++ utils/unzip.py
@@ -0,0 +1,9 @@
+import zipfile
+import os
+
+def extract_zip(zip_path, extract_to):
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+
+    # Ritorna la cartella dove sono le immagini
+    return extract_to
